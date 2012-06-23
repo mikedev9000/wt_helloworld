15ef80b3b641103286f1aebc7724efc57bb9dd24
Mike Wilson <michael1.cis@gmail.com>
2012-06-23 09:57:26 -0400

    Configured the project with the needed libraries and paths

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lboost_date_time -lboost_filesystem -lboost_thread -lboost_system -lboost_signals -lboost_regex -lwthttp -lwt
+
